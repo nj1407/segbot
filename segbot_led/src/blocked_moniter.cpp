ef14e0481487bea4b0aaa6e3b415b9e248db578f
--replace-all <brahmasp@utexas.edu>
2016-11-10 17:21:38 -0600

    added status

--- segbot_led/src/blocked_moniter.cpp
+++ segbot_led/src/blocked_moniter.cpp
@@ -26,6 +26,7 @@
 ********************************************************/
 #include "bwi_msgs/QuestionDialog.h"
 #include "bwi_services/SpeakMessage.h"
+#include "actionlib_msgs/GoalStatus.h"
 
 /*******************************************************
 *                 Global Variables                     *
@@ -35,12 +36,19 @@ ros::Publisher signal_pub;
 ros::Subscriber global_path;
 ros::Subscriber robot_pose;
 ros::Subscriber status;
+ros::Subscriber robot_goal;
 
 nav_msgs::Path current_path;
 geometry_msgs::Pose current_pose;
 
+//edit
+actionlib_msgs::GoalStatus r_goal;
+//end edit
+
+
 bool heard_path = false;
 bool heard_pose = false;
+bool heard_goal = false;
 
 /*******************************************************
 *                 Callback Functions                   *
@@ -52,6 +60,20 @@ void path_cb(const nav_msgs::Path::ConstPtr& msg)
     heard_path = true;
 }
 
+// Updates the current goal
+//update status
+//TODO empty intially so can't be blocked
+//else check what status code is
+//status code 1 moving in progress
+//status code 4 is failed to get a planned
+//status code 3 is code succeded
+
+void status_cb(const actionlib_msgs::GoalStatus::ConstPtr& msg_goal)
+{
+    r_goal = *msg_goal;
+    heard_goal = true;
+}
+
 // Updates the current pose
 void pose_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
 {
@@ -60,16 +82,7 @@ void pose_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
     heard_pose = true;
 }
 
-//update status
-//TODO empty intially so can't be blocked
-//else check what status code is
-//status code 1 moving in progress
-//status code 4 is failed to get a planned
-//status code 3 is code succeded
-void status_cb(const actionlib_msgs::status)
-{
-    
-}
+
 
 int main(int argc, char **argv)
 {
@@ -88,14 +101,16 @@ int main(int argc, char **argv)
     // Sets up subscribers
     global_path = n.subscribe("/move_base/GlobalPlanner/plan", 1, path_cb);
     robot_pose = n.subscribe("/amcl_pose", 1, pose_cb);
-    status = n.subsribe("/move_base/status", 1, status_cb); 
+    robot_goal = n.subscribe("/move_base/status", 1, status_cb); 
+    
+    
     
     // Sets up action client
     actionlib::SimpleActionClient<bwi_msgs::LEDControlAction> ac("led_control_server", true);
     ac.waitForServer();
     
     bwi_msgs::LEDControlGoal goal;
-    move_base_msgs::MoveBaseGoal goal;
+    //move_base_msgs::MoveBaseGoal goal;
     // Waits for current path and pose to update
     while(!heard_path || !heard_pose) 
     {
@@ -123,8 +138,10 @@ int main(int argc, char **argv)
         // TODO: Choose max dist rather then halving plan
         // Iterates through the first half of points in the global path and determines if any major
         // changes in orientation are coming.
+        ROS_INFO("r_goal status: %d", r_goal.status);
         if(current_path.poses.size() == 0){
-                    
+                  // && r_goal.status == 4  
+                  
                     gui_srv.request.type = 0; 
                     gui_srv.request.message = "Blocked"; 
                     gui_client.call(gui_srv);
